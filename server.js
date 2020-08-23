c223a825c93fa1740b36d7c59a8e349255356a24
rom <filpoyma@gmail.com>
2020-08-23 17:08:41 +0300

    fixed errors && npm updete

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const http = require('http');
-const app = require('./app');
+const app = require("./app");
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer(app);
-
-server.listen(port);
+app.listen(port, () => {
+  console.log("Server started at http://localhost:%s/", port);
+});
